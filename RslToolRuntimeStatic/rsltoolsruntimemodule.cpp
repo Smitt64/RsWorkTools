9ab234e4a79ec7687970da2f798d7a2ea57a5a88
Smitt64 <smitt64@yandex.ru>
2024-08-03 20:00:54 +0300

    Add rx module

--- RslToolRuntimeStatic/rsltoolsruntimemodule.cpp
+++ RslToolRuntimeStatic/rsltoolsruntimemodule.cpp
@@ -1,15 +1,17 @@
 // This is an independent project of an individual developer. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 #include "rsltoolsruntimemodule.h"
 #include "rslmodule/toolsruntimemodule.h"
 #include "rsscript/registerobjlist.hpp"
+#include "rslmodule/regexpmodule.h"
 
 QStringList RslToolsRuntimeModule::staticModules() const
 {
-    return { "ToolsRuntime" };
+    return { "ToolsRuntime", "RegExp" };
 }
 
 void RslToolsRuntimeModule::registerStaticModules()
 {
     addStaticModule<ToolsRuntimeModule>(new ToolsRuntimeModule());
+    addStaticModule<RegExpModule>(new RegExpModule());
 }
